b170b98a27b9050a38fcabf81f803dc02af7aefc
Luca Ciotti <luca.ciotti@gmail.com>
2023-10-26 08:41:33 +0200

    Fix Conteggio Imballo Robot

--- resources/views/ibp/_exports/pdf/tasksStatIMB-ROBOT.blade.php
+++ resources/views/ibp/_exports/pdf/tasksStatIMB-ROBOT.blade.php
@@ -42,23 +42,28 @@
                 <th width='15px'>Tot.</th>
             </tr>
             @foreach ($stats['imb_dim'] as $imb_dim)
+            @php
+                if(str_contains($imb_dim,'780')){
+                    continue;
+                }
+            @endphp
             <tr>
                 <th width='100px'>{{ $imb_dim }}</th>
                 @if ($imb_dim=='700 X 1500')
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '750 X 1500')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() + 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1535 X H 2200')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() +
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1530 X H 1160')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count()
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '750 X 1500')!==false; })->count() + 
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1535 X H 2200')!==false; })->count() +
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1530 X H 1160')!==false; })->count()
                         }}
                     </td>
                     @endforeach
                     <td>
                         {{ 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '750 X 1500')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() + 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1535 X H 2200')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() +
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1530 X H 1160')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count()
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '750 X 1500')!==false; })->count() + 
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1535 X H 2200')!==false; })->count() +
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '785 X 1530 X H 1160')!==false; })->count()
                         }}
                     </td>
                 @endif
@@ -67,15 +72,15 @@
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1135')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() +
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return Str::startsWith($task->ibp_imballo_tipo, '*785'); })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count()
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1135')!==false; })->count() +
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return Str::startsWith($task->ibp_imballo_tipo, '*785'); })->count()
                         }}
                     </td>
                     @endforeach
                     <td>
                         {{ 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1135')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() +
-                            $tasks->filter(function ($task) { return Str::startsWith($task->ibp_imballo_tipo, '*785'); })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count()
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1135')!==false; })->count() +
+                            $tasks->filter(function ($task) { return Str::startsWith($task->ibp_imballo_tipo, '*785'); })->count()
                         }}
                     </td>
                 @endif
@@ -84,13 +89,13 @@
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1235')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1235')!==false; })->count() 
                         }}
                     </td>
                     @endforeach
                     <td>
                         {{ 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1235')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '790 X 1540 X H 1235')!==false; })->count() 
                         }}
                     </td>
                 @endif
@@ -99,13 +104,13 @@
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '860 X 1760')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '860 X 1760')!==false; })->count() 
                         }}
                     </td>
                     @endforeach
                     <td>
                         {{ 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '860 X 1760')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '860 X 1760')!==false; })->count() 
                         }}
                     </td>
                 @endif                
@@ -114,18 +119,18 @@
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
-                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '900 X 1800 X H 1235')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return strpos($task->ibp_imballo_dim, '900 X 1800 X H 1235')!==false; })->count() 
                         }}
                     </td>
                     @endforeach
                     <td>
                         {{ 
-                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '900 X 1800 X H 1235')!==false; })->filter(function ($task) { return !in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']); })->count() 
+                            $tasks->filter(function ($task) { return strpos($task->ibp_imballo_dim, '900 X 1800 X H 1235')!==false; })->count() 
                         }}
                     </td>
                 @endif
-
-                @if ($imb_dim=='780 X 2600 X H 800')
+                
+                {{-- @if ($imb_dim=='780 X 2600 X H 800')
                     @foreach ($stats['imb_tipo'] as $item)
                     <td>
                         {{ 
@@ -153,7 +158,7 @@
                             $tasks->filter(function ($task) { return in_array($task->ibp_colonna, ['H 3000', 'H 3500']); })->count()
                         }}
                     </td>
-                @endif
+                @endif --}}
 
 
                 {{-- @if ($imb_dim!='790 X 1540 X H 1135')
@@ -178,6 +183,59 @@
                     <th>{{ $tasks->count() }}</th>
                 </tr>
             </tfoot>
+
+            <tr>
+                <td colspan="{{ count($stats['imb_tipo'])+2 }}"></td>
+            </tr>
+            <tr>
+                <th colspan="{{ count($stats['imb_tipo'])+2 }}">
+                    IMBALLO 2
+                </th>
+            </tr>
+
+            @foreach ($stats['imb_dim'] as $imb_dim)
+            @php
+            if(!str_contains($imb_dim,'780')){
+            continue;
+            }
+            @endphp
+            <tr>
+                <th width='100px'>{{ $imb_dim }}</th>
+            
+                @if ($imb_dim=='780 X 2600 X H 800')
+                @foreach ($stats['imb_tipo'] as $item)
+                <td>
+                    {{
+                    $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return in_array($task->ibp_colonna, ['H
+                    2500', 'H 3000', 'H 3500']); })->count()
+                    }}
+                </td>
+                @endforeach
+                <td>
+                    {{
+                    $tasks->filter(function ($task) { return in_array($task->ibp_colonna, ['H 2500', 'H 3000', 'H 3500']);
+                    })->count()
+                    }}
+                </td>
+                @endif
+            
+                @if ($imb_dim=='*Prolunga 780 X 2600')
+                @foreach ($stats['imb_tipo'] as $item)
+                <td>
+                    {{
+                    $tasks->where('ibp_imballo_tipo', $item)->filter(function ($task) { return in_array($task->ibp_colonna, ['H
+                    3000', 'H 3500']); })->count()
+                    }}
+                </td>
+                @endforeach
+                <td>
+                    {{
+                    $tasks->filter(function ($task) { return in_array($task->ibp_colonna, ['H 3000', 'H 3500']); })->count()
+                    }}
+                </td>
+                @endif
+            </tr>
+            @endforeach
         </table>
     </div>
 
