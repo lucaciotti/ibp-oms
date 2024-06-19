bdf0f6126805433b941c7c6f58e75c17b9204583
Luca Ciotti <luca.ciotti@gmail.com>
2024-06-19 17:00:59 +0200

    Fix Robot impianti

--- resources/views/ibp/_exports/pdf/tasksStatIMP-ROBOT.blade.php
+++ resources/views/ibp/_exports/pdf/tasksStatIMP-ROBOT.blade.php
@@ -64,14 +64,14 @@
                 <th>{{ $col }}</th>
                 @foreach ($stats['prods'] as $item)
                     <td>{{ $tasks->where('ibp_colonna', $col)->where('ibp_prodotto_tipo', $item)->count() + $tasks->where('ibp_colonna', $col)->filter(function ($task) {
-                    return str_contains($task->ibp_prodotto_tipo, '-SF-') || str_contains($task->ibp_prodotto_tipo, '-NT-');
+                    return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, '-SF-') || str_contains($task->ibp_prodotto_tipo, '-NT-'));
                     })->count() }}</td>
                     <td>{{ $tasks->where('ibp_colonna', $col)->filter(function ($task) { 
-                        return str_contains($task->ibp_prodotto_tipo, 'TOUCH') || str_contains($task->ibp_prodotto_tipo, '-ST-') || str_contains($task->ibp_prodotto_tipo, '-NE-');
+                        return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, 'TOUCH') || str_contains($task->ibp_prodotto_tipo, '-ST-') || str_contains($task->ibp_prodotto_tipo, '-NE-'));
                     })->count() }}</td>
                     @if ($item=='NEOS') 
                     <td>{{ $tasks->where('ibp_colonna', $col)->filter(function ($task) { 
-                        return str_contains($task->ibp_prodotto_tipo, '-V2-');
+                        return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, '-V2-'));
                     })->count() }}</td>
                     @endif
                     {{-- QUI SOTTO RIGA TOTALI --}}
@@ -90,14 +90,14 @@
                 <th>{{ $braccio }}</th>
                 @foreach ($stats['prods'] as $item)
                     <td>{{ $tasks->where('ibp_braccio', $braccio)->where('ibp_prodotto_tipo', $item)->count() + $tasks->where('ibp_colonna', $col)->filter(function ($task) {
-                    return str_contains($task->ibp_prodotto_tipo, '-SF-') || str_contains($task->ibp_prodotto_tipo, '-NT-');
+                    return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, '-SF-') || str_contains($task->ibp_prodotto_tipo, '-NT-'));
                     })->count() }}</td>
                     <td>{{ $tasks->where('ibp_braccio', $braccio)->filter(function ($task) {
-                    return str_contains($task->ibp_prodotto_tipo, 'TOUCH') || str_contains($task->ibp_prodotto_tipo, '-ST-') || str_contains($task->ibp_prodotto_tipo, '-NE-');
+                    return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, 'TOUCH') || str_contains($task->ibp_prodotto_tipo, '-ST-') || str_contains($task->ibp_prodotto_tipo, '-NE-'));
                     })->count() }}</td>
                     @if ($item=='NEOS')
                     <td>{{ $tasks->where('ibp_braccio', $braccio)->filter(function ($task) {
-                        return str_contains($task->ibp_prodotto_tipo, '-V2-');
+                        return str_contains($task->ibp_prodotto_tipo, $item) && (str_contains($task->ibp_prodotto_tipo, '-V2-'));
                         })->count() }}</td>
                     @endif
                     {{-- QUI SOTTO RIGA TOTALI --}}
