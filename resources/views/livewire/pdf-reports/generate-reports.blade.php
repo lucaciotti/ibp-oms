1dc844a8dfbab4a11735896c77043c589b5bae9d
Luca Ciotti <luca.ciotti@gmail.com>
2025-02-11 19:59:08 +0100

    Fix Apertura PDF firefox

--- resources/views/livewire/pdf-reports/generate-reports.blade.php
+++ resources/views/livewire/pdf-reports/generate-reports.blade.php
@@ -12,6 +12,7 @@
     <x-slot name="buttons">
         {{-- <button type="button" class="btn btn-default float-left" wire:click="$emit('modal.close')">Cancella</button> --}}
         <div></div>
+        <a class="btn btn-warning float-right" target="_blank" href="{{ asset('storage/tmp_pdf/'.$pdfReport) }}">Apri PDF</a>
         <button type="submit" class="btn btn-primary float-right">Chiudi</button>
     </x-slot>
     <style>    
@@ -26,5 +27,12 @@
             $('.pdf').parent().parent().children('.modal-header').children('.close').children().hide();
             // $('.close').prop("disabled", true);
         });
+        // const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+        
+        // if (isFirefox) {
+        // console.log("Your browser is Firefox");
+        // } else {
+        // console.log("Your browser is not Firefox");
+        // }
     </script>
 </x-wire-elements-pro::bootstrap.modal>
